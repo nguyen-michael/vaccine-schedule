c012ad9aa66b4f7806dd896c7b5452edfe46219c
unknown <nguyen.michl@gmail.com>
2020-05-13 00:59:52 -0500

    Clear boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <p>blank</p>
+    <date-of-birth-entry />
+    <vaccine-entry />
+    <vaccine-panel />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import DateOfBirthEntry from "./components/DateOfBirthEntry.vue";
+import VaccineEntry from "./components/VaccineEntry.vue";
+import VaccinePanel from "./components/VaccinePanel.vue";
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    DateOfBirthEntry,
+    VaccineEntry,
+    VaccinePanel
   },
 
   data: () => ({
