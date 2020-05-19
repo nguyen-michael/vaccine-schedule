cecc103e1ce4e17ed8e73886b9c119239c56c598
unknown <nguyen.michl@gmail.com>
2020-05-18 23:19:26 -0500

    Implement two-way on VaccineEntry

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
   <v-app>
-    <p>blank</p>
-    <date-of-birth-entry />
-    <vaccine-entry />
-    <vaccine-panel />
+    <p class="display-1 text-center">Vax Check!</p>
+    <v-expansion-panels multiple>
+      <the-date-of-birth-entry 
+        :date-of-birth="dateOfBirth" 
+        @change:date="updateDOB" 
+      />
+    </v-expansion-panels>
+    <v-card>
+      <v-card-title class="my-2" >
+        Enter dates for received doses:
+      </v-card-title>
+    </v-card>
+    <v-expansion-panels multiple>
+      <vaccine-entry 
+        v-for="(vaccine, i) in this.vaccines" 
+        :key="vaccine.name" 
+        :vaccine="vaccine" 
+        :date-of-birth="dateOfBirth"
+        :index="i"
+        @update:variant="updateVariant"
+        @update:date-received="updateDatesReceived"
+      />
+      <vaccine-panel />
+    </v-expansion-panels>
   </v-app>
 </template>
 
 <script>
-import DateOfBirthEntry from "./components/DateOfBirthEntry.vue";
+import TheDateOfBirthEntry from "./components/TheDateOfBirthEntry.vue";
 import VaccineEntry from "./components/VaccineEntry.vue";
 import VaccinePanel from "./components/VaccinePanel.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    DateOfBirthEntry,
+    TheDateOfBirthEntry,
     VaccineEntry,
     VaccinePanel
   },
 
   data: () => ({
-    //
+    dateOfBirth: new Date().toLocaleString('sv').substr(0, 10),
+    vaccines: [
+      {
+        name: "HiB",
+        variants: [
+          "PRP-OMP (Pedvax)",
+          "PRP-T (ActHIB, Hiberex, Pentacel)",
+          "Both/Unknown"
+        ],
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "HepA",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "HepB",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "IPV",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "Rotavirus",
+        variants: ["Rotateq", "Rotarix", "Both/Unknown"],
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "DTAP",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "Varicella",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "MMR",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      },
+      {
+        name: "PCV13",
+        variants: null,
+        variant: null,
+        datesReceived: []
+      }
+    ]
   }),
+
+  methods: {
+    updateDOB(date) {
+      this.dateOfBirth = date;
+    },
+
+    updateVariant(variant, index) {
+      this.vaccines[index].variant = variant;
+    },
+
+    updateDatesReceived(dateList, index) {
+      this.vaccines[index].datesReceived = dateList;
+    }
+  }
 };
 </script>
--- src/components/VaccineEntry.vue
+++ src/components/VaccineEntry.vue
@@ -1,45 +1,83 @@
 <template>
-  <form>
-    <fieldset>
-      <label for="hepatitis-a">HepA</label>
-      <div>
-        <input type="date" name="hepatitis-a" id="hepA"/>
-        <input type="submit" value="Add Date"/>
-      </div>
-      <div>
-        <table>
-          <thead>
-            <th>Dose Number</th>
-            <th>Dose Date</th>
-            <th>Delete</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2020-02-12</td>
-              <td>
-                <button>Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>2020-05-11</td>
-              <td>
-                <button>Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </fieldset>
-  </form>
+  <v-expansion-panel>
+    <v-expansion-panel-header>{{ vaccine.name }}</v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-select
+        v-if="vaccine.variants"
+        :items="vaccine.variants"
+        v-model="variant"
+        label="Choose variant received"
+        @change="handleAddVariant"
+      ></v-select>
+      <v-date-picker
+        v-model="date"
+        :reactive="true"
+        :min="dateOfBirth"
+        :max="new Date().toLocaleString('sv').substr(0, 10)"
+      >
+        <v-btn text color="primary" @click="handleAddDate()">Add Vaccination Date</v-btn>
+      </v-date-picker>
+      <v-list dense>
+        <v-subheader>Selected Dates</v-subheader>
+        <v-list-item 
+          v-for="(date, i) in dateList" 
+          :key="i"
+        >
+          <v-list-item-content>
+            <span>{{ date }}<v-btn outlined small :value="i" @click="handleDeleteDate">Delete</v-btn></span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
 export default {
-  name: "VaccineEntry"
+  name: "VaccineEntry",
+
+  props: {
+    vaccine: Object,
+    dateOfBirth: String,
+    index: Number
+  },
+
+  data: () => ({
+    date: new Date().toLocaleString('sv').substr(0, 10),
+    dateList: [],
+    variant: null
+  }),
+
+  methods: {
+    handleAddDate() {
+      // Date validations
+      if (this.date < this.dateOfBirth) {
+        console.log("Can't get a shot before you're born.");
+        return;
+      }
+
+      if (this.dateList.includes(this.date)) {
+        alert("You've already given a shot on this date.");
+        return;
+      }
+
+      if (this.dateList.length > 5) {
+        alert("That's probably too many shots.");
+        return;
+      }
+      
+      this.dateList = [...this.dateList, this.date].sort();
+      this.$emit("update:date-received", this.dateList, this.index);
+    },
+
+    handleAddVariant() {
+      this.$emit("update:variant", this.variant, this.index);
+    },
+
+    handleDeleteDate(dateIndex) {
+      this.dateList.splice(dateIndex, 1);
+      this.$emit("update:date-received", this.dateList, this.index);
+    }
+  }
 };
 </script>
-
-<style scoped>
-</style>
